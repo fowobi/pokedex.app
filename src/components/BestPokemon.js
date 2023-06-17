7e3a75312ec0766f1eea1357055bd7fbc451f3ad
fowobi <109865505+fowobi@users.noreply.github.com>
2023-06-17 15:39:10 +0100

    Update BestPokemon.js

--- src/components/BestPokemon.js
+++ src/components/BestPokemon.js
@@ -1,20 +1,3 @@
-// import React from "react";
-
-// function BestPokemon() {
-//   const abilities = ["Anticipation", "Adaptability", "Run-Away"];
-//   return (
-//     <div>
-//       <p>My favorite pokemon is squirtle</p>
-//       <ul>
-//         {abilities.map((name, index) => (
-//           <li key={index}>{name} </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default BestPokemon;
 
 import React from "react";
 
